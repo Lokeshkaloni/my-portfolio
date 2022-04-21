ae7d565807383b51218bb6fb02eb0d9676d7f041
lokesh kaloni <lokeshkaloni@gmail.com>
2022-04-21 13:00:12 +0530

    commit

--- src/NavBAr.jsx
+++ src/NavBAr.jsx
@@ -3,7 +3,7 @@ import MyLink from "./MyLink";
 function NavBar() {
   return (
     <div className="bg-gray-600 sticky top-0 justify-between px-8 py-1 w-screen h-10 flex">
-      <MyLink to="/home">HOME</MyLink>
+      <MyLink to="/home">ImHome</MyLink>
       <MyLink to="/aboutme">ABOUT ME</MyLink>
       <MyLink to="/myworks">MY WORKS</MyLink>
       <MyLink to="/contactme">CONTACT</MyLink>

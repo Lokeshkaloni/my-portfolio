66e355cce2462ec5bd1d95c1f34c0b744dfe6cff
lokesh kaloni <lokeshkaloni@gmail.com>
2022-04-21 20:50:36 +0530

    added myimage in home page

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -5,7 +5,10 @@ import Button from "./Button";
 function Homepage() {
   return (
     <div className="flex flex-col justify-center items-center h-screen w-screen bg-black space-y-4">
-      <img className="rounded-full object-cover h-52 w-52 border-gray-500 border-2" />
+      <img
+        src="https://instagram.fdel1-3.fna.fbcdn.net/v/t51.2885-15/279035992_724263248584247_4712528805270631967_n.jpg?stp=dst-jpg_e15_fr_s1080x1080&cb=9ad74b5e-8af86364&_nc_ht=instagram.fdel1-3.fna.fbcdn.net&_nc_cat=111&_nc_ohc=GsohA6ZGFgsAX9fKYUT&edm=ABJHkxYAAAAA&ccb=7-4&ig_cache_key=MjgyMTM3NjQyMzUxMjUyODI1Nw%3D%3D.2-ccb7-4&oh=00_AT-Hr1raYkNHtVWg3kLK_rKY0kPZQxejL1lXr4tlBO0Ptg&oe=6269024D&_nc_sid=fa978c"
+        className="rounded-full object-cover h-52 w-52 border-gray-500 border-2"
+      />
       <div className="flex flex-col justify-center items-center">
         <h1 className="text-yellow-500 text-3xl font-bold">
           I'M LOKESH KALONI
